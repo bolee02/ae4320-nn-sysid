a42157965c8adbe06834b2e48925b03d9ab0f618
bolee02 <bolee02@gmail.com>
2024-06-03 15:15:25 +0200

    initial files

--- kalman_filter.py
+++ kalman_filter.py
@@ -16,5 +16,3 @@ train_data = pd.read_csv('data/F16traindata_CMabV_2024.csv')
 C_m = train_data.iloc[:, 0].to_numpy()
 Z_k = train_data.iloc[:, 1:4].to_numpy()  # alpha_m, beta_m, V_m
 U_k = train_data.iloc[:, 4:7].to_numpy()  # u_dot, v_dot, w_dot
-
-print(C_m.size())
